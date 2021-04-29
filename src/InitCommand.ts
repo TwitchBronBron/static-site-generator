50af89bedb65c0b56e24f28fdb8a0563f059e43e
Bronley <bronley@gmail.com>
2021-04-29 00:01:24 -0400

    Add cli 'init' command

---
+++ src/InitCommand.ts
@@ -0,0 +1,53 @@
+import * as fsExtra from 'fs-extra';
+import * as path from 'path';
+import * as isEmptyDir from 'empty-dir';
+import * as latestVersion from 'latest-version';
+import * as childProcess from 'child_process';
+
+export class InitCommand {
+
+    public async run(options: { path: string }) {
+        let outDir: string;
+        if (options.path) {
+            outDir = path.resolve(process.cwd(), options.path);
+        } else {
+            outDir = process.cwd();
+        }
+        if (fsExtra.pathExistsSync(outDir) && !isEmptyDir.sync(outDir)) {
+            throw new Error(`Directory is not empty: ${outDir}`);
+        }
+
+        //copy the files to `src`
+        fsExtra.copySync(
+            path.resolve(__dirname, '..', 'templates', 'default'),
+            path.resolve(outDir, 'src')
+        );
+
+        //create the package.json
+        fsExtra.outputFileSync(
+            path.resolve(outDir, 'package.json'),
+            JSON.stringify({
+                name: 'statigen-site',
+                version: '0.1.0',
+                description: 'A static website built by statigen',
+                scripts: {
+                    build: 'statigen',
+                    watch: 'statigen --watch'
+                },
+                dependencies: {
+                    statigen: `^${'0.1.0' ?? await latestVersion('statigen')}`
+                }
+            }, null, 4)
+        );
+
+        //run npm install
+        childProcess.spawnSync(
+            process.platform.startsWith('win') ? 'npm.cmd' : 'npm',
+            ['install'],
+            {
+                cwd: outDir,
+                stdio: 'inherit'
+            }
+        );
+    }
+}
--- src/cli.ts
+++ src/cli.ts
@@ -2,35 +2,46 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 
 import * as yargs from 'yargs';
+import { InitCommand } from './InitCommand';
 import type { Options } from './StaticSiteGenerator';
 import { StaticSiteGenerator } from './StaticSiteGenerator';
 yargs
     .usage('$0', 'A simple convention-based static site generator for markdown files')
     .help('help', 'View help information about this tool.')
-    .option('sourceDir', {
-        description: `Path to the directory containing your source files`,
-        alias: 's',
-        type: 'string',
-        default: 'src'
-    })
-    .option('outDir', {
-        description: `Path to the directory where the output file should be written.`,
-        alias: 'o',
-        type: 'string',
-        default: 'dist'
-    })
-    .option('cwd', {
-        description: `Path to the current working directory where the tool should run.`,
-        type: 'string'
-    })
-    .option('watch', {
-        description: `If true, the program will run in watch mode and rebuild on every source file change. This also starts a web server to host the generated site.`,
-        type: 'boolean',
-        default: false
-    })
-    .command('$0 [files..]', '', () => { }, (argv: any) => {
+    .command('$0 [files..]', '', (yargs: yargs.Argv) => {
+        return yargs
+            .option('sourceDir', {
+                description: `Path to the directory containing your source files`,
+                alias: 's',
+                type: 'string',
+                default: 'src'
+            })
+            .option('outDir', {
+                description: `Path to the directory where the output file should be written.`,
+                alias: 'o',
+                type: 'string',
+                default: 'dist'
+            })
+            .option('cwd', {
+                description: `Path to the current working directory where the tool should run.`,
+                type: 'string'
+            })
+            .option('watch', {
+                description: `If true, the program will run in watch mode and rebuild on every source file change. This also starts a web server to host the generated site.`,
+                type: 'boolean',
+                default: false
+            });
+    }, (argv: any) => {
         const generator = new StaticSiteGenerator();
         generator.run(argv as Options).catch((e) => {
+            console.error(e);
+            process.exit(1);
+        });
+    })
+    .command('init [path]', '', () => { }, (argv: any) => {
+        const initCommand = new InitCommand();
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        initCommand.run(argv).catch(e => {
             console.error(e);
             process.exit(1);
         });
